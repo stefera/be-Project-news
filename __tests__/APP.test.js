8934577f2b10f886a4f76bf4a36024ab9196201b
Samuel Tefera <samueltefera@Sarahs-MBP.home>
2023-02-21 17:23:04 +0000

    task 5 started

--- __tests__/APP.test.js
+++ __tests__/APP.test.js
@@ -61,3 +61,46 @@ describe("task3- GET api/topics", () => {
       });
   });
 });
+
+describe("task5- GET api/articles/:articleiD", () => {
+  test("returns a single article object when id exists", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        // console.log(body);
+        const originalData = topicData;
+        expect(body).not.toBe(originalData);
+        expect(originalData).not.toBe(body);
+        expect(Array.isArray(body)).toBe(true);
+      });
+  });
+  test("returns a correct array of all topics, each with a correct slug and description", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        const originalData = topicData;
+
+        body.forEach((item) => {
+          expect(item).toMatchObject({
+            slug: expect.any(String),
+            description: expect.any(String),
+          });
+          expect(body.length).toBe(3);
+        });
+        expect(body).toEqual(originalData);
+      });
+  });
+
+  test("responds with an appropriate error message when topics endpoint is misspelt", () => {
+    return request(app)
+      .get("/api/nottopics")
+      .expect(404)
+      .then((result) => {
+        console.log("error result body", result.body);
+        expect(result.status).toBe(404);
+        expect(result.body.msg).toBe("Invalid path name, try again");
+      });
+  });
+});
