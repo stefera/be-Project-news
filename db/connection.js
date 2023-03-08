2d27ad9c673776bbe0be5c84174aca2bb3371b7d
Samuel Tefera <samueltefera@Sarahs-MBP.home>
2023-03-08 14:23:28 +0000

    misc

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,5 @@
-const { config } = require("dotenv");
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
-
-require("dotenv").config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
-
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PGDATABASE not set");
-}
 
 const config =
   ENV === "production"
@@ -17,6 +8,13 @@ const config =
         max: 2,
       }
     : {};
+require("dotenv").config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE not set");
+}
 
 module.exports = new Pool();
 module.exports = new Pool(config);
